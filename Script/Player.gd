0a35eaf29605476c71983804ec32e2f1770c50b9
ssnuke <49909116+ssnuke@users.noreply.github.com>
2020-05-01 20:42:09 +0530

    new scene

--- Script/Player.gd
+++ Script/Player.gd
@@ -1,22 +1,46 @@
 extends KinematicBody2D
 
 const SPEED = 520
-const GRAVITY = 500
+const GRAVITY = 250
 const UP = Vector2(0,-1)
-const JUMP_SPEED = 250
+const JUMP_SPEED = 2000
 
 var motion = Vector2()
 
 
 func _physics_process(delta):
-	var dir = 0
-	motion.y += GRAVITY * delta
-	if Input.is_action_pressed("move_right"):
-		dir +=1
-	if Input.is_action_pressed("move_left"):
-		dir -=1
-	if Input.is_action_pressed("Jump") and is_on_floor():
+	gravity()
+	move()
+	animate()
+
+
+func move():
+	if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
+		motion.x = +SPEED
+	else:
+		motion.x =0
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
-	
-	motion.x = SPEED * dir
-	motion = move_and_slide(motion,UP)
+	move_and_slide(motion,UP)
+
+
+func gravity():
+	if not is_on_floor():
+		motion.y += GRAVITY
+	else:
+		motion.y = 0
+
+
+func animate():
+	if motion.y <0:
+		$AnimatedSprite.play("jump")
+	elif motion.x > 0:
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = false
+	elif motion.x <0:
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.play("idel")
